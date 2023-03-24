a77d3eba160f07768d81aa1bc84ca003fa9c698a
Matheus Henrique Machado <matheus.thosi@gmail.com>
2023-03-23 23:26:48 -0300

    commit inicial

---
+++ portfolio.js
@@ -0,0 +1,48 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.projetos-container', { origin: 'bottom' });
+ScrollReveal().reveal('.inicio-content, span' , { origin: 'top' });
+ScrollReveal().reveal('.inicio-content h1', { origin: 'left' });
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Desenvolvedor Front-end', 'UX/UI Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
